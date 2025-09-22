124e35dc6e6cd0090bf4a29080be702c475fdc9d
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-22 19:15:24 +0200

    add mousemove event

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const slogan = document.getElementById("slogan");
 console.log(slogan);
+
+let x = 0;
+let y = 0;
+
+slogan.addEventListener("mousemove", (e) => {
+  x = e.clientX;
+  y = e.clientY;
+  console.log(x, y);
+});
